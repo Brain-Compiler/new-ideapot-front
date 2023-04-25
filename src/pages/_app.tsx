787b4cf79f6e0018e6b6f31a5e8462767f46df3f
limjh5699 <limjh5699@gmail.com>
2023-04-25 15:54:22 +0900

    FIX: Global Style Path Error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
-import "@/styles/globals.css";
+import "../styles/globals.css";
+
 import AppLayout from "@/components/layout/appLayout";
 
 export default function App({ Component, pageProps }: AppProps) {

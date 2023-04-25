ec2930e90d26738c52404b5fe152ee1aed2c3f33
limjh5699 <limjh5699@gmail.com>
2023-04-25 17:00:50 +0900

    FEAT: Add Section Template for Main Page

---
+++ src/components/main/main/sectionTemplate/index.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+import styled from "styled-components";
+
+import Phone from "@/assets/pages/main/img/common/phone.png";
+
+type propsType = {
+  category: string;
+  title: string;
+  description: string;
+};
+
+const SectionTemplate = ({ category, title, description }: propsType) => {
+  return (
+    <Background>
+      <Contents>
+        <Info>
+          <Category>{category}</Category>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+        </Info>
+        <Image src={Phone} alt="Phone" style={ImageStyle} />
+      </Contents>
+    </Background>
+  );
+};
+
+const Background = styled.section`
+  width: 100%;
+  height: 78.75rem;
+  background-color: #fff;
+`;
+
+const Contents = styled.div`
+  width: 80rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div``;
+
+const Category = styled.span`
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: #9776ff;
+`;
+
+const Title = styled.h1`
+  font-size: 2.75rem;
+  font-weight: 700;
+  margin-top: 3.125rem;
+  margin-bottom: 1.875rem;
+  white-space: pre-line;
+`;
+
+const Description = styled.span`
+  font-size: 1.5rem;
+  color: #a5a5a5;
+  white-space: pre-line;
+`;
+
+const ImageStyle = {
+  width: "37.5rem",
+  height: "58.5rem",
+};
+
+export default SectionTemplate;
